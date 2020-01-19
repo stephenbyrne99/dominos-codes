0cb6db80cd2b92e8286cdcd75034fc05282de923
Stephen Byrne <39441007+stephenbyrne99@users.noreply.github.com>
2020-01-19 15:58:50 +0000

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+
+//components
+import Footer from './components/footer';
+
+//libraries
+import styled from 'styled-components';
+
+
+//images
+import logo from './logo.png';
+
+//styles
 import './App.css';
+
+const H1 = styled.h1`
+  
+`;
+
+const Code = styled.h2`
+  font-style: italic;
+`;
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <H1>Dominos Code Ireland</H1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Latest code: </p>
+        <Code>JANDOM20</Code>
+        <Footer />
       </header>
     </div>
   );
---
+++ src/components/footer.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+//libraries
+import styled from 'styled-components';
+
+
+//images
+
+//styles
+import '.././App.css';
+
+const Img = styled.img`
+  height:50px;
+`;
+
+
+const Footer: React.FC = () => {
+  return (
+    <div className="Footer">
+          <p>Developed by <a href="https://www.linkedin.com/in/stephen-byrne-429267167/" target="_blank" rel="noopener noreferrer">Stephen Byrne</a></p>
+          <div className="row">
+            <a href="https://www.instagram.com/stephenbyrne99/" target="_blank" rel="noopener noreferrer"><Img alt = "instagram" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fe%2Fe7%2FInstagram_logo_2016.svg%2F1200px-Instagram_logo_2016.svg.png&f=1&nofb=1"/></a>
+            <a href="https://www.linkedin.com/in/stephen-byrne-429267167/" target="_blank" rel="noopener noreferrer"><Img alt = "linkedin" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fc%2Fc9%2FLinkedin.svg%2F1200px-Linkedin.svg.png&f=1&nofb=1"/></a>
+          </div>
+    </div>
+  );
+}
+
+export default Footer;
